59791de4e16beb18287c6c1ada5022407e05bfa6
ujjwal chauhan <ujjwalchauhan@ujjwals-MacBook-Air.local>
2023-05-10 10:43:37 +0530

    Initial commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL);
+const DB = 'mongodb+srv://Uj293:Abc123456@cluster0.dvn94rc.mongodb.net/Task-Manager-Api?retryWrites=true&w=majority'
+mongoose.connect(DB).then(()=>{
+    console.log('Connection is successful')
+}).catch((err)=>{
+    console.log('no connection')
+});
 
 
